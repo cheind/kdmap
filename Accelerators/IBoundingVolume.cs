0150f930fce47c6d3423debf4dfda11a407e84df
christoph.heindl <christoph.heindl@4703ada4-51dc-11de-8e0a-c97bf73df9f4>
2009-06-06 07:10:25 +0000

    Introduced IBoundingVolume interface for queries to be implemented. Added implementation of a hypersphere and tests.

---
+++ Accelerators/IBoundingVolume.cs
@@ -0,0 +1,48 @@
+//  
+//  Copyright (C) 2009 Christoph Heindl
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+
+using System;
+
+namespace Accelerators
+{
+  
+  /// <summary>
+  /// Interface that allows bounding volume queries.
+  /// </summary>
+  public interface IBoundingVolume
+  {
+    /// <value>
+    /// Access the dimensionality of the bounding volume.
+    /// </value>
+    int Dimensions {
+      get;
+    }
+    
+    /// <summary>
+    /// Test if vector is contained in bounding volume.
+    /// A vector lying on the bounding volume's surface is considered to be contained.
+    /// </summary>
+    bool Inside(IVector x);
+    
+    /// <summary>
+    /// Test if bounding volume intersects with the given axis aligned bounding box.
+    /// </summary>
+    bool Intersect(AABB aabb);
+    
+    
+  }
+}
