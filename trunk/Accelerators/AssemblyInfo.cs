bf9ff336fa11456b8a97952359769e59ca50b7bc
christoph.heindl <christoph.heindl@4703ada4-51dc-11de-8e0a-c97bf73df9f4>
2009-06-06 07:12:36 +0000

    assembly info

--- trunk/Accelerators/AssemblyInfo.cs
+++ trunk/Accelerators/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Christoph Heindl")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
