2a322a925b50093d0d0629f3fb17af27704ec0a4
christoph.heindl <christoph.heindl@4703ada4-51dc-11de-8e0a-c97bf73df9f4>
2009-06-09 19:18:07 +0000

    documentation added

--- trunk/Accelerators/PriorityQueue.cs
+++ trunk/Accelerators/PriorityQueue.cs
@@ -22,10 +22,22 @@ using System.Text;
 namespace Accelerators {
 
   /// <summary>
-  /// Min-Priority queue implementation.
+  /// A min priority queue
   /// </summary>
+  /// 
+  /// <remarks>
+  /// Implementation is based on a heap-like data structure stored in an array. 
+  /// When the initial capacity is reached the queue dynamically grows by doubling the size of the internal data structure.
+  /// The queue never releases acquired memory until finalization. 
+  /// 
+  /// By default the queue is a min-priority queue. That is, the top element is the smallest according the the comparer used.
+  /// To change this behaviour simply exchange the comparer.
+  /// </remarks>
   public class PriorityQueue<TPriority, TValue> {
 
+    /// <summary>
+    /// Default constructor. Requires that a comparer for TPriority exists.
+    /// </summary>
     public PriorityQueue() {
       _comp = Comparer<TPriority>.Default;
       _heap = null;
@@ -33,6 +45,9 @@ namespace Accelerators {
       this.Capacity = 31;
     }
 
+    /// <summary>
+    /// Construct with custom comparer.
+    /// </summary>
     public PriorityQueue(IComparer<TPriority> comparer) {
       _comp = comparer;
       _heap = null;
@@ -41,18 +56,24 @@ namespace Accelerators {
     }
 
     /// <summary>
-    /// Access the capacity. 
+    /// Access the capacity. The returned capacity might deviate
+    /// from the previously set capacity, because the heap is stored as
+    /// a full balanced tree in array form which requires to round up the
+    /// supplied capacity to the next possible value.
     /// </summary>
+    /// <remarks>
+    /// The capacity is calculated from the given value, v, by solving for x in
+    /// 2^x - 1 >= v, where x is constraint to be an integer value.
+    /// </remarks>
     public int Capacity {
       get {
         return _heap.Length;
       }
       set {
-        // Heap is stored as full balanced tree: we need to find
-        // (2^x - 1) >= capacity.
+        // Heap is stored as full balanced tree
         double x = Math.Ceiling(Math.Log(value + 1) / Math.Log(2));
         int capacity = ((int)Math.Pow(2, x)) - 1;
-        // Allocate and copy
+        // Allocate and copy if required
         Pair<TPriority, TValue>[] newHeap = new Pair<TPriority, TValue>[capacity];
         if (_heap != null) {
           Array.Copy(_heap, 0, newHeap, 0, _count);
@@ -61,12 +82,18 @@ namespace Accelerators {
       }
     }
 
+    /// <value>
+    /// Access the number of elements in the queue 
+    /// </value>
     public int Count {
       get {
         return _count;
       }
     }
 
+    /// <summary>
+    /// Enqueue an element with priority
+    /// </summary>
     public void Push(TPriority p, TValue v) {
       if (_count == this.Capacity) {
         this.Capacity = 2 * this.Capacity + 1;
@@ -75,6 +102,9 @@ namespace Accelerators {
       this.UpHeap(_count - 1, new Pair<TPriority, TValue>(p, v));
     }
 
+    /// <summary>
+    /// Remove the top element according to the comparer used.
+    /// </summary>
     public TValue Pop() {
       if (_count == 0)
         throw new InvalidOperationException("Cannot pop from empty priority queue.");
@@ -85,12 +115,18 @@ namespace Accelerators {
       return first;
     }
 
+    /// <summary>
+    /// Peek at the top element
+    /// </summary>
     public TValue Peek() {
       if (_count == 0)
         throw new InvalidOperationException("Cannot pop from empty priority queue.");
       return _heap[0].Second;
     }
 
+    /// <summary>
+    /// Peek at the top priority 
+    /// </summary>
     public TPriority PeekPriority() {
       if (_count == 0) {
         throw new InvalidOperationException("Cannot pop from empty priority queue.");
@@ -98,25 +134,42 @@ namespace Accelerators {
       return _heap[0].First;
     }
 
+    /// <summary>
+    /// Up-heap the entry by starting comparison with the parent of the supplied index.
+    /// </summary>
     private void UpHeap(int index, Pair<TPriority, TValue> entry) {
       int parent = (index - 1) / 2;
 
+      // While there is a parent and the priority of the parent greater than the priority of the supplied entry
       while ((index > 0) && (_comp.Compare(_heap[parent].First, entry.First) > 0)) {
+        // down-shuffle the parent
         _heap[index] = _heap[parent];
+        // continue search at parent
         index = parent;
+        // compare with new parent
         parent = (index - 1) / 2;
       }
+      // at this point index marks the position of the entry
       _heap[index] = entry;
     }
 
+    /// <summary>
+    /// Down-Heap 
+    /// </summary>
     private void DownHeap(int index, Pair<TPriority, TValue> entry) {
+      // First set to left child of index to be removed.
       int child = (index * 2) + 1; // left child
+      // As long as we haven't reached [past] the last child.
       while (child < _count) {
+        // If the right child is in reach and the priority of the right child is less than the priority of the left child
+        // then move to the left child.
         if ((child + 1 < _count) && (_comp.Compare(_heap[child].First, _heap[child + 1].First) > 0)) {
           child += 1;
         }
+        // Move the lesser child upward
         _heap[index] = _heap[child];
         index = child;
+        // Goto left child
         child = (index * 2) + 1;
       }
       this.UpHeap(index, entry);
