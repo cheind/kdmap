5923fd82b41cce5cf5cea730ca0fad9372ebb247
christoph.heindl <christoph.heindl@4703ada4-51dc-11de-8e0a-c97bf73df9f4>
2009-06-22 05:46:28 +0000

    Refactored all issue tests to custom class

---
+++ trunk/AcceleratorsTests/Issues/Issue3.cs
@@ -0,0 +1,48 @@
+//  
+//  Copyright (C) 2009 Christoph Heindl
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Accelerators;
+using Accelerators.Subdivision;
+
+namespace AcceleratorsTests.Issues
+{
+  /// <summary>
+  /// Issue #3: Trivial splits might cause endless recursion
+  /// http://code.google.com/p/kdmap/issues/detail?id=3
+  /// </summary>
+  [TestFixture()]
+	public class Issue3
+	{
+    [Test]
+    public void TestTrivialSplitsForEndlessRecursion() {
+      // Test when split bounds is non-empty, but all contained points are degenerate
+      KdNode<IVector> n = new KdNode<IVector>();
+      n.Vectors = new List<IVector>(new IVector[] { Vector.Create(-1, -5), Vector.Create(-1, -5) });
+      n.InternalBounds = new AABB(Vector.Create(-1.25, -5), Vector.Create(-1, -5));
+      n.InternalBounds = new AABB(2);
+      n.InternalBounds.Enlarge<IVector>(n.Vectors);
+      AxisOfMaximumSpreadSelector aom = new AxisOfMaximumSpreadSelector();
+      PeriodicAxisSelector pas = new PeriodicAxisSelector();
+      NUnitExtensions.Assert.Throws(typeof(DegenerateDatasetException), delegate { aom.Select(n); });
+      NUnitExtensions.Assert.Throws(typeof(DegenerateDatasetException), delegate { pas.Select(n); });
+    }
+	}
+}
